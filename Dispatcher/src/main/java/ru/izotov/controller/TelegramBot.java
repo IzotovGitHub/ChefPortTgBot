403dbaf74a2cf9a70fb4369ab1470aeffe0b322f
izotov_a <aleksandr.izotov2020@gmail.com>
2023-02-05 19:43:22 +0300

    init commit

---
+++ Dispatcher/src/main/java/ru/izotov/controller/TelegramBot.java
@@ -0,0 +1,44 @@
+package ru.izotov.controller;
+
+import lombok.extern.log4j.Log4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+@Log4j
+@Component(value = "chef-port.bot")
+public class TelegramBot extends TelegramLongPollingBot {
+
+    @Value("${bot.name}")
+    private String botName;
+
+    @Value("${bot.token}")
+    private String botToken;
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        log.debug(update.getMessage().getText());
+    }
+
+    @Bean
+    public TelegramBotsApi getTelegramBotsApi() throws TelegramApiException {
+        TelegramBotsApi api = new TelegramBotsApi(DefaultBotSession.class);
+        api.registerBot(this);
+        return api;
+    }
+
+    @Override
+    public String getBotUsername() {
+        return botName;
+    }
+
+    @Override
+    public String getBotToken() {
+        return botToken;
+    }
+}
