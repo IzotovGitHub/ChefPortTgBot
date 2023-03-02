5187366d0f01f1690e4a0f356315cf3b18e4ed55
izotov_a <aleksandr.izotov2020@gmail.com>
2023-03-02 20:23:47 +0300

    bot-2 Добавил новую очередь

--- Node/src/main/java/ru/izotov/configuration/RabbitConfiguration.java
+++ Node/src/main/java/ru/izotov/configuration/RabbitConfiguration.java
@@ -1,15 +1,27 @@
 package ru.izotov.configuration;
 
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import ru.izotov.config.RabbitMqConfig;
+
 
 @Configuration
 public class RabbitConfiguration {
 
+    @Autowired
+    private RabbitMqConfig rabbitMqConfig;
+
     @Bean
     public MessageConverter jsonMessageConverter() {
         return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public Queue authQueue() {
+        return new Queue(rabbitMqConfig.getAuthUserQueue());
     }
 }
