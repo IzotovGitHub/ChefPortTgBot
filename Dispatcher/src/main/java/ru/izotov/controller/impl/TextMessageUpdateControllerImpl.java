993cd8870a8a055697effd65e3232c895e2e63c2
izotov_a <aleksandr.izotov2020@gmail.com>
2023-03-03 22:20:04 +0300

    bot-2 мелкие правки

--- Dispatcher/src/main/java/ru/izotov/controller/impl/TextMessageUpdateControllerImpl.java
+++ Dispatcher/src/main/java/ru/izotov/controller/impl/TextMessageUpdateControllerImpl.java
@@ -3,12 +3,12 @@ package ru.izotov.controller.impl;
 import lombok.AllArgsConstructor;
 import lombok.NonNull;
 import lombok.extern.log4j.Log4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import ru.izotov.config.RabbitMqConfig;
 import ru.izotov.controller.UpdateController;
-import ru.izotov.service.UpdateProducer;
 
 import static com.google.common.base.Preconditions.checkArgument;
 
@@ -16,9 +16,9 @@ import static com.google.common.base.Preconditions.checkArgument;
 @Component
 @AllArgsConstructor
 public class TextMessageUpdateControllerImpl implements UpdateController {
-    private final UpdateProducer updateProducer;
     private final RabbitMqConfig rabbitMqConfig;
 
+    private final RabbitTemplate rabbitTemplate;
 
     @Override
     public void process(@NonNull Update update) {
@@ -26,7 +26,7 @@ public class TextMessageUpdateControllerImpl implements UpdateController {
         Message message = update.getMessage();
 
         if (message.hasText()) {
-            updateProducer.produce(rabbitMqConfig.getTextQueue(), update);
+            rabbitTemplate.convertAndSend(rabbitMqConfig.getTextQueue(), update);
         } else {
             log.warn("Unsupported message is received: " + update);
         }
