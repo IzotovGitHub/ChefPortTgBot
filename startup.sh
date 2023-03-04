e200cbb12aa4f5e4896326e2bb0df4b237cd8292
izotov_a <aleksandr.izotov2020@gmail.com>
2023-03-04 15:20:52 +0300

    bot-2 Дополнил скрипт запуска приложения

--- startup.sh
+++ startup.sh
@@ -3,6 +3,8 @@
 VERSION=1.0-SNAPSHOT
 DISPATCHER_PATH=Dispatcher
 NODE_PATH=Node
+AUTH_SERVICE_PATH=Auth-Service
 
 java -jar ./${NODE_PATH}/target/Node-${VERSION}.jar &
+java -jar ./${AUTH_SERVICE_PATH}/target/Auth-Service-${VERSION}.jar &
 java -jar ./${DISPATCHER_PATH}/target/Dispatcher-${VERSION}.jar;
